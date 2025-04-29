93305f56fc2d83d3d872e1fe9e2ceaaef1ea5c09
Jeferson Braine <jefeleal363@gmail.com>
2025-04-29 19:32:55 -0300

    add: Principles, js project POKEDEX

---
+++ FirstPagesJS/Functions/typesFunction.js
@@ -0,0 +1,22 @@
+//Tipos de função
+
+//Declarativas
+    // function funcao() {
+    //     console.log("Fução declarativa")
+    // }
+// funcao()
+
+//Expressões de Funções
+//com nomeação
+    // let funcao = function funcao() {
+    //     console.log("Função de expressão")
+    // }
+    // funcao()
+
+//se tirar o "funcao" logo após o function ainda funciona
+
+//Arrow function
+    //let funcao = () => {
+    //console.log("arrow function")
+//     }
+// funcao();
