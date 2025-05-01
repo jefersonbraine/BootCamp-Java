075f2e2f87c2c7009d32aca48ed4199d6a1c5e1b
Jeferson Braine <jefeleal363@gmail.com>
2025-05-01 19:59:49 -0300

    fix: now pokemon sorted via id no order

--- POKEDEX/assets/js/main.js
+++ POKEDEX/assets/js/main.js
@@ -1,7 +1,10 @@
 const pokemonList = document.getElementById("pokemonList");
 const loadMoreButton = document.getElementById("loadMoreButton");
+const maxRecords = 11
 const limit = 5;
 let offset = 0;
+
+
 
 function loadPokemonItens(offset, limit) {
 	pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
