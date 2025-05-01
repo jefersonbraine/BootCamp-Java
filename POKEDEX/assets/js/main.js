640c3cdac8646fab837a1d0c882959ae0cff183e
Jeferson Braine <jefeleal363@gmail.com>
2025-05-01 19:44:17 -0300

    add: Pagination mechanism

--- POKEDEX/assets/js/main.js
+++ POKEDEX/assets/js/main.js
@@ -1,29 +1,37 @@
-// function convertPokemonTypesToLi(pokemonTypes) {
-// 	return pokemonTypes.map((typeSlot) =>
-// 		`<li class="type">${type}</li>`
-// 	);
-// }
+const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
+const limit = 5;
+let offset = 0;
 
-function convertPokemonToLi(pokemon) {
-	return `
-    <li class="pokemon ${pokemon.type}">
-        <span class="number">#${pokemon.number}</span>
-        <span class="name">${pokemon.name}</span>
+function loadPokemonItens(offset, limit) {
+	pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+		pokemonList.innerHTML += pokemons
+			.map(
+				(pokemon) =>
+					`
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
 
-        <div class="detail">
-            <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-            </ol>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types
+											.map((type) => `<li class="type ${type}">${type}</li>`)
+											.join("")}
+                </ol>
 
-            <img src="${pokemon.photo}" alt="${pokemon.name}">
-        </div>
-    </li>    
-    
-    `;
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+        </li>    
+        `
+			)
+			.join("");
+	});
 }
 
-const pokemonList = document.getElementById("pokemonList");
+loadPokemonItens(offset, limit);
 
-pokeApi.getPokemons().then((pokemons = []) => {
-	pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
+loadMoreButton.addEventListener("click", () => {
+	offset += limit;
+	loadPokemonItens(offset, limit);
 });
