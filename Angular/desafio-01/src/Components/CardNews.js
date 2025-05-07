917b6b13bf4d691390901b24bb5f47b1f365b148
Jeferson Braine <jefeleal363@gmail.com>
2025-05-07 19:14:22 -0300

    add: Building components and set the properties

--- Angular/desafio-01/src/Components/CardNews.js
+++ Angular/desafio-01/src/Components/CardNews.js
@@ -1,10 +1,39 @@
-class cardNews extends HTMLElement {
+class CardNews extends HTMLElement {
 	constructor() {
 		super();
 
 		const shadow = this.attachShadow({ mode: "open" });
-		shadow.innerHTML = "<h1> Hello World</h1>";
+		shadow.appendChild(this.build());
+		shadow.appendChild(this.styles());
 	}
+	build() {
+		const componentRoot = document.createElement("div");
+		componentRoot.setAttribute("class", "card");
+
+		const cardLeft = document.createElement("div");
+		cardLeft.setAttribute("class", "card_left");
+
+		const autor = document.createElement("span");
+		const linkTitle = document.createElement("a");
+		const newsContent = document.createElement("p");
+
+		cardLeft.appendChild(autor);
+		cardLeft.appendChild(linkTitle);
+		cardLeft.appendChild(newsContent);
+
+		const cardRight = document.createElement("div");
+		cardRight.setAttribute("class", "card_right");
+
+		const newsImage = document.createElement("img");
+		cardRight.appendChild(newsImage);
+
+		componentRoot.appendChild(cardLeft);
+		componentRoot.appendChild(cardRight);
+
+		return componentRoot;
+	}
+
+	styles() {}
 }
 
-customElements.define("card-news", cardNews);
+customElements.define("card-news", CardNews);
