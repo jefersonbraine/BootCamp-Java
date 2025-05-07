9b1b158926cc6e98817979956522cfe19e49085c
Jeferson Braine <jefeleal363@gmail.com>
2025-05-07 19:53:25 -0300

    fnsh: Creating style for the components

--- Angular/desafio-01/src/Components/CardNews.js
+++ Angular/desafio-01/src/Components/CardNews.js
@@ -32,7 +32,8 @@ class CardNews extends HTMLElement {
 
 		const newsImage = document.createElement("img");
 		cardRight.appendChild(newsImage);
-		newsImage.src = this.getAttribute("photo") || "assets/images/no-image-found.png";
+		newsImage.src =
+			this.getAttribute("photo") || "assets/images/no-image-found.png";
 		newsImage.alt = "Foto da Noticia";
 
 		componentRoot.appendChild(cardLeft);
@@ -41,7 +42,65 @@ class CardNews extends HTMLElement {
 		return componentRoot;
 	}
 
-	styles() {}
+	styles() {
+		const style = document.createElement("style");
+		style.textContent = `
+
+		* {
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+			font-family: Helvetica, sans-serif;
+		}
+		.card {
+			display: flex;
+			flex-direction: row;
+			width: 80%;
+			margin-top: 40px;
+			justify-content: space-between;
+			box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+			-webkit-box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+			-moz-box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+		}
+
+
+		.card_left {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			padding-left: 10px;
+		}
+
+		.card_left > span {
+			font-weight: 500;
+		}
+
+		.card_left > a {
+			color: black;
+			text-decoration: none;
+			font-weight: bold;
+			margin-top: 10px;
+			font-size: 30px;
+		}
+
+		.card_left > p {
+			color: rgb(75, 72, 72);
+			font-size: 20px;
+			font-weight: 300;
+		}
+
+
+		.card_right img {
+			width: 400px;   
+			margin-bottom: -5px;
+		}
+
+				
+				
+		`;
+
+		return style;
+	}
 }
 
 customElements.define("card-news", CardNews);
