5bcb8956568a82fb2ede79cb2f5ce459759e50b8
Jeferson <jefeleal363@gmail.com>
2025-05-12 20:45:43 -0300

    att: content java package Operadores

--- Santander Bootcamp - Fullstack Java + Angular/Java/java-anatomia/src/edu/jeferson/operadores/Operadores.java
+++ Santander Bootcamp - Fullstack Java + Angular/Java/java-anatomia/src/edu/jeferson/operadores/Operadores.java
@@ -2,12 +2,44 @@ package edu.jeferson.operadores;
 
 public class Operadores {
     public static void main(String[] args) {
-        int numero = 5;
-        numero = - numero;
-        System.out.println(numero);
+//        int numero = 5;
+//        numero = - numero;
+//        System.out.println(numero);
+//
+//        numero = numero * -1;
+//        System.out.println(numero);
 
-        numero = numero * -1;
-        System.out.println(numero);
+
+        String nomeUm = "GLEYSON";
+        String nomeDois = new String("GLEYSON");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+
+        int numero1 = 2;
+        int numero2 = 1;
+
+        if (numero1 > numero2) // true
+            System.out.println("maior");
+
+        boolean simNao = numero1 == numero2;
+        System.out.println("numeroUm é igual ao numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+        System.out.println("numeroUm é maior que o numeroDois? " + simNao);
+
+        simNao = numero1 < numero2;
+        System.out.println("numeroUm é menor que o numeroDois? " + simNao);
+
+        simNao = numero1 >= numero2;
+        System.out.println("numeroUm é maior ou igual ao numeroDois? " + simNao);
+
+        simNao = numero1 <= numero2;
+        System.out.println("numeroUm é menor ou igual ao numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+        System.out.println("numeroUm é diferente do numeroDois? " + simNao);
+
 
     }
 }
