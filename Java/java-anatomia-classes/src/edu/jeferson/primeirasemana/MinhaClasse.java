92ac7919b3285c2fe7226987aa7286fe6f6b064b
Jeferson <jefeleal363@gmail.com>
2025-05-12 14:58:23 -0300

    add: how manage and create the packages

--- Java/java-anatomia-classes/src/edu/jeferson/primeirasemana/MinhaClasse.java
+++ Java/java-anatomia-classes/src/edu/jeferson/primeirasemana/MinhaClasse.java
@@ -1,3 +1,5 @@
+package edu.jeferson.primeirasemana;
+
 public class MinhaClasse {
 
    public static void main(String[] args) {
--- Java/java-anatomia-classes/src/edu/jeferson/segundasemana/BoletimEstudantil.java
+++ Java/java-anatomia-classes/src/edu/jeferson/segundasemana/BoletimEstudantil.java
@@ -1,3 +1,5 @@
+package edu.jeferson.segundasemana;
+
 public class BoletimEstudantil {
     public static void main(String[] args) {
         int mediaFinal = 7;
