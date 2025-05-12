aac3ff632b589582fa9cd2f5839c6a29b9155933
Jeferson <jefeleal363@gmail.com>
2025-05-12 14:41:50 -0300

    add: how ident work in Java <3

---
+++ Java/java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,11 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 7;
+        if(mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if(mediaFinal == 6)
+            System.out.print("PROVA MINERVA");
+        else
+            System.out.print("APROVADO");
+    }
+}
