d156c4380966230ea21714a8f62e5a0925b56666
Jeferson <jefeleal363@gmail.com>
2025-05-11 21:15:31 -0300

    update: README and First method

--- Java/java-anatomia-classes/src/MinhaClasse.java
+++ Java/java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,15 @@
 public class MinhaClasse {
-    public static void main(String[] arg) {
-        System.out.println("Hello World!");
-    }
+
+   public static void main(String[] args) {
+    String firstName = "Jeferson";
+    String secondName = "Braine";
+
+    String nomeCompleto = nomeCompleto(firstName, secondName);
+    System.out.println(nomeCompleto);
+   }
+
+   public static String nomeCompleto(String firstName, String secondName) {
+       return "Resultado do método: " + firstName.concat(" ").concat(secondName);
+   }
+
 }
