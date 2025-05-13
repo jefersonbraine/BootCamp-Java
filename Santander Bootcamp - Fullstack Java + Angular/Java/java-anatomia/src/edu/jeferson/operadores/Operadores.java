d14f3a7fc0ae1c7ad3bd44d3973ae920ad2cee6f
Jeferson <jefeleal363@gmail.com>
2025-05-12 21:08:30 -0300

    update: finished operators module and att README_JAVA

--- Santander Bootcamp - Fullstack Java + Angular/Java/java-anatomia/src/edu/jeferson/operadores/Operadores.java
+++ Santander Bootcamp - Fullstack Java + Angular/Java/java-anatomia/src/edu/jeferson/operadores/Operadores.java
@@ -1,5 +1,7 @@
 package edu.jeferson.operadores;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Operadores {
     public static void main(String[] args) {
 //        int numero = 5;
@@ -10,36 +12,61 @@ public class Operadores {
 //        System.out.println(numero);
 
 
-        String nomeUm = "GLEYSON";
-        String nomeDois = new String("GLEYSON");
+//        String nomeUm = "GLEYSON";
+//        String nomeDois = new String("GLEYSON");
+//
+//        System.out.println(nomeUm.equals(nomeDois));
+//
+//
+//        int numero1 = 2;
+//        int numero2 = 1;
+//
+//        if (numero1 > numero2) // true
+//            System.out.println("maior");
+//
+//        boolean simNao = numero1 == numero2;
+//        System.out.println("numeroUm é igual ao numeroDois? " + simNao);
+//
+//        simNao = numero1 > numero2;
+//        System.out.println("numeroUm é maior que o numeroDois? " + simNao);
+//
+//        simNao = numero1 < numero2;
+//        System.out.println("numeroUm é menor que o numeroDois? " + simNao);
+//
+//        simNao = numero1 >= numero2;
+//        System.out.println("numeroUm é maior ou igual ao numeroDois? " + simNao);
+//
+//        simNao = numero1 <= numero2;
+//        System.out.println("numeroUm é menor ou igual ao numeroDois? " + simNao);
+//
+//        simNao = numero1 != numero2;
+//        System.out.println("numeroUm é diferente do numeroDois? " + simNao);
 
-        System.out.println(nomeUm.equals(nomeDois));
+//        boolean variavel = true;
+//
+//        variavel = !variavel;
+//
+//        System.out.println(variavel);
 
+//        int a, b;
+//        a = 10;
+//        b = 20;
+//
+//        String resultado = a == b ? "Verdadeiro" : "Falso";
+//        System.out.println(resultado);
 
-        int numero1 = 2;
-        int numero2 = 1;
+        boolean condicao1, condicao2;
+        condicao1 = true;
+        condicao2 = false;
 
-        if (numero1 > numero2) // true
-            System.out.println("maior");
+        if (condicao1 && condicao2) {
+            System.out.println("As duas são verdadeiras");
+        }
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das duas são verdadeiras");
+        }
 
-        boolean simNao = numero1 == numero2;
-        System.out.println("numeroUm é igual ao numeroDois? " + simNao);
-
-        simNao = numero1 > numero2;
-        System.out.println("numeroUm é maior que o numeroDois? " + simNao);
-
-        simNao = numero1 < numero2;
-        System.out.println("numeroUm é menor que o numeroDois? " + simNao);
-
-        simNao = numero1 >= numero2;
-        System.out.println("numeroUm é maior ou igual ao numeroDois? " + simNao);
-
-        simNao = numero1 <= numero2;
-        System.out.println("numeroUm é menor ou igual ao numeroDois? " + simNao);
-
-        simNao = numero1 != numero2;
-        System.out.println("numeroUm é diferente do numeroDois? " + simNao);
-
+        System.out.println("fim");
 
     }
 }
